354876b2c50702761c5ee1af6391320db0797bd0
webgrity70 <webgrity70@gmail.com>
2021-12-07 14:55:51 +0530

    header chnages

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
+<html <?php language_attributes(); ?>>
+<head class="no-js">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php wp_head();?>
 </head>
 <body <?php body_class(); ?> >
-
+<?php wp_body_open(); ?>
 <header class="header">
     <div class="container-fluid bg-green topHeader py-3">
         <div class="container container1225">
