b2b2a2fe884313fea2d0a8581443059018cbc369
web-ram <webgrity70@gmail.com>
2022-01-24 12:03:30 +0530

    format update

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-
-
 get_header(); ?>
 
-
-<main class="otherPages newsList"  id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 
--- page.php
+++ page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+<main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 	<?php the_content(); ?>
@@ -9,5 +10,6 @@
 
 		<p>Sorry, no posts matched your criteria.</p>
 
-<?php endif; ?>
+	<?php endif; ?>
+</main>
 <?php get_footer(); ?>
