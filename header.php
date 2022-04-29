12f9540ff0e582fe733598e2430e58713ed093c6
web-ram <webgrity70@gmail.com>
2022-04-29 15:13:39 +0530

    head class moved to HTML tag

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html class="no-js" <?php language_attributes(); ?>>
 
-<head class="no-js">
+<head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php wp_head();?>
