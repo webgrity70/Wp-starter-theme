57c7a2d44e5c1872bc214e4ea1ef038397abcf36
web-ram <webgrity70@gmail.com>
2022-05-02 18:56:12 +0530

    $ funtion removed from files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,47 +1,6 @@
-$(document).ready(function(){
+jQuery(document).ready(function($){
 
 
-    var height = $('.topHeader').outerHeight();
-
-    $(window).scroll(function() {
-        if($(this).scrollTop() > height)
-        {
-            $(".header").addClass("fixedHeader");
-            
-        }
-        else if($(this).scrollTop() <= height)
-        {
-             $(".header").removeClass("fixedHeader");
-            
-        }     
-    });
-
-
-
-    $("#homecarousel").carousel({
-        interval: 6000,
-    });
-
-
-    $(".footerFormCont .wpcf7-form-control").focus(function(){
-        $(this).parent().parent().find(".subLabel").children('span').fadeOut(200);
-    }).blur(function() {
-        var inputVal = $(this).val();
-        if ( inputVal=="" ||  inputVal==" " ) {
-            $(this).parent().parent().find(".subLabel").children('span').fadeIn(200);
-        }
-    });
-
-
-    $(".footerFormCont .wpcf7-form-control").each(function( index ) {
-      /*console.log( index + ": " + $( this ).val() );*/
-        if( !$( this ).val()  ){
-            $(this).parent().parent().find(".subLabel").children('span').fadeIn(200);
-        }
-        else{
-            $(this).parent().parent().find(".subLabel").children('span').fadeOut(200);
-        }
-    });
 
 
     var menu = new MmenuLight(
